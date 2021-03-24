2e3ca104a8b6c5009528b9d20414b8b3ec6d7d18
哭啊 <fafa87@gmail.com>
2021-03-24 16:57:21 +0800

    Initial Commit

---
+++ OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC
+//
+//  Created by Cat Hite on 24/03/2021.
+//  Copyright © 2021 Cat Hite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
